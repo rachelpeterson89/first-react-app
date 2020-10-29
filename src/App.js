bef9cefb0741303129a84efa498e972c8cad5d39
Rachel Peterson <rachelp5446@gmail.com>
2020-10-29 13:24:38 -0500

    PersonCard changes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import PersonCard from './components/PersonCard';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello, Rachel!</h1>
-        <h2>Things I need to do:</h2>
-        <ul>
-          <li>Learn React</li>
-          <li>Climb Mt. Everest</li>
-          <li>Run a marathon</li>
-          <li>Feed the Dogs</li>
-        </ul>
-      </header>
+      <PersonCard firstName={"Jane"} lastName={"Doe"} age={45} hairColor={"Black"}/>
+      <PersonCard firstName={"John"} lastName={"Smith"} age={88} hairColor={"Brown"}/>
+      <PersonCard firstName={"Millard"} lastName={"Fillmore"} age={50} hairColor={"Brown"}/>
+      <PersonCard firstName={"Maria"} lastName={"Smith"} age={62} hairColor={"Brown"}/>
     </div>
-  );
+  )
 }
 
 export default App;
