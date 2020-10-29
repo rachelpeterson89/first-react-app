8ef35a78c4622f7d444ae1cbf913f288ed694e8a
Rachel Peterson <rachelp5446@gmail.com>
2020-10-29 14:11:58 -0500

    added button

--- src/components/PersonCard.jsx
+++ src/components/PersonCard.jsx
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
 
 class PersonCard extends Component {
+
+    constructor(props) {
+        super(props);
+            this.state = {
+                changeAge : props.age
+            }
+    }
+    
     render() {
         return (
             <div>
                 <h1>{this.props.lastName}, {this.props.firstName}</h1>
-                <h3>Age: {this.props.age}</h3>
+                <h3>Age: {this.state.changeAge}</h3>
                 <h3>Hair Color: {this.props.hairColor}</h3>
+
+                <button onClick={ () => this.setState({changeAge: this.state.changeAge + 1})}>One Year Older</button>
             </div>
-        ) 
+        );
     }
 }
 
